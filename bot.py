5085883b20279a19a8ad79ff0f24d4b461eda4af
Gabrielle Co <gabriellejco@gmail.com>
2022-01-29 17:27:00 -0600

    reply functionality

--- bot.py
+++ bot.py
@@ -1,16 +1,16 @@
 import tweepy
 
 from config import consumer_key, consumer_secret, access_token, access_token_secret, bearer_token
-from reply import searchTweets
+from reply import replyTweets
 
 client = tweepy.Client(consumer_key = consumer_key,
                     consumer_secret = consumer_secret,
                     access_token = access_token,
                     access_token_secret = access_token_secret,
                     bearer_token= bearer_token)
-                    
+
 ####Replying
-searchTweets(client)
+replyTweets(client)
 
 
 
--- reply.py
+++ reply.py
@@ -1,13 +1,16 @@
+from time import sleep
 import tweepy
 
 #Search tweets that @ the account and have #activitiesjsc2022
-def searchTweets(client):
+def replyTweets(client):
   query = '@hellospace101 #activitiesjsc2022 -is:retweet'  #Don't include retweets
+  tweets = client.search_recent_tweets(query=query, tweet_fields=['created_at'], expansions = 'author_id', max_results=10)
 
-  tweets = client.search_recent_tweets(query=query, tweet_fields=['context_annotations', 'created_at'], max_results=10)
-  print(tweets)
-
-#Reply to tweet
-
+  for tweet in tweets.data:
+    authorId = tweet.author_id
+    userName = (client.get_user(id = authorId)).data
+    status = '@' + str(userName) + ' i am replying again'
+    client.create_tweet(text = status, in_reply_to_tweet_id = tweet.id)
+    sleep(10) 
 
 #Stretch: reply to tweet based on command (similar to discord bot)
