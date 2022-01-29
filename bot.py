fabdf377c574d57dee08ef0944336f8f12d401fa
Gabrielle Co <gabriellejco@gmail.com>
2022-01-29 14:19:11 -0600

    fix

--- bot.py
+++ bot.py
@@ -2,10 +2,10 @@ import tweepy
 
 from config import consumer_key, consumer_secret, access_token, access_token_secret
 
-client = tweepy.Client(consumer_key,
-                    consumer_secret,
-                    access_token,
-                    access_token_secret)
-
+client = tweepy.Client(consumer_key = consumer_key,
+                    consumer_secret = consumer_secret,
+                    access_token = access_token,
+                    access_token_secret = access_token_secret)
+                    
 # Replace the text with whatever you want to Tweet about
-response = client.create_tweet(text='hello world2')
+response = client.create_tweet(text='hello world3')
