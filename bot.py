83bb5625f71253678630848eec38b746fae1b85d
Gabrielle Co <gabriellejco@gmail.com>
2022-01-29 14:07:02 -0600

    key update

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 import tweepy
 
-client = tweepy.Client(consumer_key="3Bh4BzDfe40ciAKHIvwx5Pehe",
-                    consumer_secret="sIEZlTUeQLxACnsiOrp4rsh8kD1uEFoph7WI27kUmOAj6mQEoV",
-                    access_token="1484434372061188096-lU0VPJyP2FSajDfbiwqt8TYvJdSCYA",
-                    access_token_secret="zQX3w6LfUbpSHn0X8VH6ilyZg4ZRkEws6KCtjC7reJDi3")
-                    
+from config import consumer_key, consumer_secret, access_token, access_token_secret
+
+client = tweepy.Client(consumer_key,
+                    consumer_secret,
+                    access_token,
+                    access_token_secret)
+
 # Replace the text with whatever you want to Tweet about
-response = client.create_tweet(text='hello world')
+response = client.create_tweet(text='hello world2')
