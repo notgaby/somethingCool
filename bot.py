57d5032e5c4cc4a2f1c0ee7112037a29ffb412d5
Gabrielle Co <gabriellejco@gmail.com>
2022-01-30 00:50:00 -0600

    env

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 import tweepy
+import os
 from time import sleep
 #from config import consumer_key, consumer_secret, access_token, access_token_secret, bearer_token
 from reply import replyTweets, getActivity
 
-client = tweepy.Client(consumer_key = consumer_key,
-                    consumer_secret = consumer_secret,
-                    access_token = access_token,
-                    access_token_secret = access_token_secret,
-                    bearer_token= bearer_token)
+client = tweepy.Client(consumer_key = os.environ['consumer_key'],
+                    consumer_secret = os.environ['consumer_secret'],
+                    access_token = os.environ['access_token'],
+                    access_token_secret = os.environ['access_token_secret'],
+                    bearer_token= os.environ['bearer_token'])
 
                     
 # Replace the text with whatever you want to Tweet about
